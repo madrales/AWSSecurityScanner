095f095047098bf44cdb92e1c8bf1324438ac925
Mason Adrales <masonadrales@Masons-MacBook-Pro.local>
2026-06-02 11:51:52 -0500

    Implementing state and encryption check

--- ebs_checks.py
+++ ebs_checks.py
@@ -1,3 +1,29 @@
+import boto3
+
 def run_checks():
-    print("Hello World!")
+    accessPoint = boto3.client('ec2')
+    volumes = accessPoint.describe_volumes()
+    print("Evaluating EBS security...\n\n")
+
+    #IMP: checking for state of volumes
+    print("\n\nChecking volumes for state == \'available\'...\n\n")
+    for x in volumes["Volumes"]:
+        if x["State"] == "available":
+            print("FAIL: \'" + x["VolumeId"] + "\' is in \'available\' state, meaning it is not attached to an instance.")
+        elif x["State"] == "in-use":
+            print("PASS: \'" + x["VolumeId"] + "\' is in state \'" + x["State"] + "\' for Instance ID \'" + x["Attachments"][0]["InstanceId"] + "\'.")
+        else:
+            print("Unable to evaluate state for volume \'" + x["VolumeId"] + "\' in Instance ID \'" + x["Attachments"][0]["InstanceId"] + "\'.")
+
+    #IMP: checking encryption of volumes
+    print("\n\nChecking volumes for encryption...")
+    for x in volumes["Volumes"]:
+        if x["Encrypted"] == False:
+            print("FAIL: \'" + x["VolumeId"] + "\' is not encrypted.")
+        elif x["Encrypted"] == True:
+            print("PASS: \'" + x["VolumeId"] + "\' is encrypted.")
+        else:
+            print("Unable to evaluate encryption for volume \'" + x["VolumeId"] + "\' in Instance ID \'" + x["Attachments"][0]["InstanceId"] + "\'.")
+
+    
 run_checks()
